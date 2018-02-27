2ad1adbf5978493c6ae345a4bc2862c06c08da49
Itzchuck <ieshachnovitz@gmail.com>
2018-02-27 13:32:25 +0200

    Require inputs to have values

--- script/script.js
+++ script/script.js
@@ -2,9 +2,21 @@ var index = getCounter();
 var task = document.getElementById('note')
 var taskDate = document.getElementById('date')
 var saveBtn = document.querySelector('div.form-paper button')
+
 saveBtn.addEventListener('click', Save)
 
 function Save() {
+    if (task.value == "" ){
+        task.required = true;
+        console.log(task.value, 'empty')
+        return
+    }else if (taskDate.value == ""){
+        taskDate.required = true;
+        return
+    }
+    else {
+        task.required = false; 
+        taskDate.required = false
         var _note = task.value;
         var _notedate = taskDate.value;
         var o = {note : _note , notedate: _notedate}
@@ -14,6 +26,7 @@ function Save() {
         localStorage.setItem("counter" , index);
 
         createTask()
+        }
     }
 
     function getCounter() {
@@ -25,6 +38,8 @@ var taskContainer = document.querySelector('.task-container')
         var newTask = document.createElement('div')
         var taskDueDate = document.createElement('div')
         var taskContent = document.createElement('p')
+        var taskHeader = document.createElement('h4')
+        var dueByHeader = document.createElement('h4')
         newTask.className = 'new-task col-s-12 col-md-4 col-lg-2'
         newTask.id = 'newTask'+index
         var xIcon = document.createElement('span')
@@ -51,15 +66,31 @@ var taskContainer = document.querySelector('.task-container')
 
 function deleteBtns(){
 var deleteBtnsArr = document.querySelectorAll('.glyphicon-remove-sign')
-for (let index = 0; index < deleteBtnsArr.length; index++) {
-    deleteBtnsArr[index].addEventListener('click', deleteTask);
+for (let i=0; i<deleteBtnsArr.length; i++) {
+    deleteBtnsArr[i].addEventListener('click', deleteTask);
     }   
 }
 function deleteTask(){
     localStorage.removeItem('note' + this.id)
+    index--
     console.log(this.id)
     var newTask = document.getElementById('newTask' + this.id)
-    newTask.outerHTML = ""
-    delete newTask
+    var xIcon = document.getElementById(this.id)
+    xIcon.outerHTML = ""
+    delete xIcon
+    newTask.className = 'new-task fade-out'
+    setTimeout(function () {
+        newTask.outerHTML = ""
+        delete newTask
+    }, 1000)
+}
 
+function loadData() {
+    if (localStorage.length > 0){
+        let key = localStorage.counter
+        // while key 
+        // localStorage.getItem(key)
+        
+    }
+    
 }
